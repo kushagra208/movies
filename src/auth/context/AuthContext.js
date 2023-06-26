702fd4763b2a980baca458b8422b931571b4de1e
kushagra208 <kushagrag2082002@gmail.com>
2023-06-26 12:40:01 +0530

    Client Component

--- src/auth/context/AuthContext.js
+++ src/auth/context/AuthContext.js
@@ -1,3 +1,4 @@
+'use client'
 import { createContext, useContext, useEffect , useState } from "react";
 import { auth , dbCollection, provider } from "../config/config.js";
 import { GoogleAuthProvider, signInWithPopup, signOut } from "firebase/auth";
